343f580935d7d390c59ea6dc7cf4441bd67b5a9c
Christopher Cuartas <ccuartas@uncc.edu>
2025-02-11 12:36:58 -0500

    added commnets to help team setup flask routing

--- app.py
+++ app.py
@@ -3,6 +3,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template('testing.html')
+
+# Sample route
+@app.route("/") #This is what will be shown in the url. '/' is the landing page
+def hello_world(): #This is the function, if you need to pass data or anything to the html page, it will be done here. For the midterm this should just contain the return function.
+    return render_template('testing.html') #render_template is used to send html to client. inside should be the name of your file that is located under the template folder
+
+# HTML should be in templates folder
+# CSS should be in static folder
+
+# Edit these
+@app.route("") 
+def hello_world(): 
+    return render_template('')
+
+@app.route("") 
+def hello_world(): 
+    return render_template('')
+
+@app.route("") 
+def hello_world(): 
+    return render_template('')

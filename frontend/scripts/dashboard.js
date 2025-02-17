0f8514e1b73143e5aadbc0c757a9fb8bd648038d
NickPetruccelli <NickPetro18@gmail.com>
2025-02-17 13:39:28 -0500

    switched to left side select bar and right desc

--- frontend/scripts/dashboard.js
+++ frontend/scripts/dashboard.js
@@ -1,18 +1,58 @@
 const api_ip = "http://127.0.0.1:5000"
+let data = null;
 main();
 
 async function main() {
-	const data = await fetch(api_ip + "/train-info", {
+	data = await fetch(api_ip + "/train-info", {
 		"method": "GET",
 	},);
 	console.log(data);
-	const vehicles = await data.json();
-	console.log(vehicles);
-	load_vehicles(vehicles);
+	data = await data.json();
+	console.log(data);
+	load_vehicle_list(data);
+	//load_vehicles(vehicles);
+}
+
+function load_vehicle_list(vehicles) {
+	const vehicle_list = document.querySelector(".vehicle-list");
+	const vehicle_desc = document.querySelector(".vehicle-desc");
+	for (const id in vehicles) {
+		const list_item = document.createElement("li");
+		const itemData = vehicles[id];
+		list_item.setAttribute("data-id", id);
+		list_item.innerText = itemData["name"];
+		list_item.addEventListener("click", list_event_handler)
+		vehicle_list.appendChild(list_item);
+		if (vehicle_desc.innerText === "") {
+			load_vehicle_desc(id);
+		}
+	}
+}
+
+function list_event_handler(e) {
+	const id = this.dataset.id;
+	load_vehicle_desc(id);
+}
+
+function load_vehicle_desc(vehicle_id) {
+	const vehicle_desc = document.querySelector(".vehicle-desc");
+	while (vehicle_desc.firstChild) {
+		vehicle_desc.removeChild(vehicle_desc.lastChild);
+	}
+	console.log(data);
+	console.log(vehicle_id);
+	const vehicle_info = data[vehicle_id];
+	console.log(vehicle_info);
+	const name = document.createElement("h2");
+	name.innerText = vehicle_info["name"];
+	const desc = document.createElement("p");
+	desc.innerText = vehicle_info["name"];
+	vehicle_desc.appendChild(name);
+	vehicle_desc.appendChild(desc);
 }
 
 function load_vehicles(vehicles) {
-	const vehicle_list = document.querySelector(".vehicle-cont");
+	const vehicle_list = document.querySelector(".vehicle-list");
 	for (const vehicle_id in vehicles) {
 		const vehicle_item = document.createElement("div");
 		vehicle_item.classList.add("vehicle");
@@ -27,7 +67,7 @@ function load_vehicles(vehicles) {
 		console.log(vehicle_id);
 		console.log(vehicles);
 		vehicle_name.innerText = vehicles[vehicle_id]["name"];
-		body_details.innerText = "asjkhdf kjhf kljahsdfkj hasdhj fahsdl";
+		load_components(body_details, vehicles[vehicle_id]["components"]);
 		vehicle_header.addEventListener("click", toggle_vehicle_body);
 
 		vehicle_header.appendChild(vehicle_name);
@@ -44,5 +84,13 @@ function toggle_vehicle_body(e) {
 		body.classList.remove("hidden");
 	} else {
 		body.classList.add("hidden");
+	}
+}
+
+function load_components(body_details, components) {
+	for (const component in components) {
+		const component_elm = document.createElement("h3");
+		body_details.appendChild(component_elm);
+		component_elm.innerText = component;
 	}
 }

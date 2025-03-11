61a2963b5af08fd0c7c75afcbd09df9121d25f0e
Christopher C <chris5cuartas@gmail.com>
2025-03-11 10:47:25 -0400

    imported all flask folders in __init__.py

--- flask-backend/api/__init__.py
+++ flask-backend/api/__init__.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint #Blueprint is used to import differnt files to flask AKA orginization
 
+#Import files in api folder
 from .frontend import frontend
 from .machine_learning import machine_learning
+from .embedded_system import embedded_system

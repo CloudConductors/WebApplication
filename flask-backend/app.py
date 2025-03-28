2fe71a977f355d10d3da3e9c1b6cec6b6edb8a1e
Devin Thompson <dthom118@charlotte.edu>
2025-03-28 14:13:07 -0400

    changed variables to be lowercase, fixed readme.md formatting

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 from flask import Flask, jsonify, render_template, request, session
 from flask_cors import CORS
@@ -10,17 +9,11 @@ import bcrypt
 import base64
 import uuid
 from datetime import datetime
-=======
-from flask import Flask
-from flask_cors import CORS
-from api import frontend, machine_learning, embedded_system #import files from api folder
->>>>>>> main
 
 app = Flask(__name__,
             template_folder='frontend',
             static_folder='frontend',
             static_url_path='')
-<<<<<<< HEAD
 CORS(app)
 
 # ~~~~~~~~~~~~~~~~~~~~~~ Sessions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -84,27 +77,27 @@ def schedule():
             return jsonify({'error': 'You are not an admin'}), 403
         try:
             maintenance = schedule_table.scan(
-                FilterExpression=Attr('Maintenance_Scheduled').eq('false')
+                FilterExpression=Attr('maintenance_scheduled').eq('false')
             )
         except ClientError as e:
             return jsonify({'error': 'BE GONE'}), 403
         try:
-            Component_Id = schedule_table.scan(
+            component_id = schedule_table.scan(
                 FilterExpression=Attr('component_id').eq('1')
             )
         except ClientError as e:
             return jsonify({'error': 'ID not found'}), 404
 
-        if 'Items' in Component_Id and len(Component_Id['Items']) > 0 and 'Items' in maintenance and len(maintenance['Items']) > 0:
+        if 'Items' in component_id and len(component_id['Items']) > 0 and 'Items' in maintenance and len(maintenance['Items']) > 0:
             try:
-                if 'Items' in Component_Id and len(Component_Id['Items']) > 0:
-                    Component_Id = Component_Id['Items'][0]['component_id']
+                if 'Items' in component_id and len(component_id['Items']) > 0:
+                    component_id = component_id['Items'][0]['component_id']
                 else:
                     return jsonify({'error': 'Component ID not found'}), 404
 
                 # Check if item exists before inserting (in case you're replacing it)
                 existing_item = schedule_table.get_item(
-                    Key={'component_id': str(Component_Id), 'Last_Repair_Date': '01/01/2001'}
+                    Key={'component_id': str(component_id), 'last_repair_date': '01/01/2001'}
                 )
                 if 'Item' not in existing_item:
                     return jsonify({'error': 'Item not found in table'}), 404
@@ -112,13 +105,14 @@ def schedule():
                 # Perform put_item (replaces the existing item with new values)
                 maintenance = schedule_table.put_item(
                     Item={
-                        'component_id': str(Component_Id),
-                        'Last_Repair_Date': '01/01/2001',
-                        'Expected_Repair_DUF': '03/25/2099',
-                        'Maintenance_Scheduled': 'true',
-                        'Manually_Overriden': 'true',
-                        'Mean_DUF': 3,
-                        'Standard_Deviation_DUF' : 12
+                        'component_id': str(component_id),
+                        'train_id': '1',
+                        'expected_repair_duf': '03/25/2099',
+                        'last_repair_date': '01/01/2001',
+                        'maintenance_scheduled': 'false',
+                        'manually_overriden': 'true',
+                        'mean_duf': 3,
+                        'standard_deviation_duf' : 12
                     }
                 )
                 print("Table updated successfully!")
@@ -326,15 +320,3 @@ def get_train_info(): #Changed from hello_world() --> get_train_info()
         },
     }
     return jsonify(trains)
-=======
-
-
-CORS(app)
-
-# ~~~~~~~~~~~~~~~~~~~~~~ API STUFF ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-#All Files in the api folder are called here
-app.register_blueprint(frontend) #This calls the fronend.py
-app.register_blueprint(machine_learning, url_prefix="/machine-learning") #This calls the , machine_learning.py and sets its url to /machine_learning
-app.register_blueprint(embedded_system) #This calls the embeded_system.py
->>>>>>> main

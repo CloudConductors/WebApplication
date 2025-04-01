fc4d3704a40201dbc41520e4ac532a9dce497f29
Christopher C <chris5cuartas@gmail.com>
2025-04-01 11:04:40 -0400

    login and signup functionality works

--- flask-backend/api/frontend.py
+++ flask-backend/api/frontend.py
@@ -120,38 +120,38 @@ def login():
     password = request.json.get('password')
     print(f"Email: {email}, Password: {password}")
     
-    return jsonify({'email': email, 'password': password}), 200
+    # return jsonify({'email': email, 'password': password}), 200
 
-    # if not email or not password:
-    #     return jsonify({'error': 'Missing required fields'}), 400
+    if not email or not password:
+        return jsonify({'error': 'Missing required fields'}), 400
     
-    # #Querying DynamoDB for the user
-    # try:
-    #     response = table.scan(
-    #         FilterExpression=Attr('acc-info.email').eq(email)
-    #     )
+    #Querying DynamoDB for the user
+    try:
+        response = table.scan(
+            FilterExpression=Attr('acc-info.email').eq(email)
+        )
         
-    #     if 'Items' not in response or len(response['Items']) == 0:
-    #         return jsonify({'error': 'Invalid credentials'}), 400
+        if 'Items' not in response or len(response['Items']) == 0:
+            return jsonify({'error': 'Invalid credentials'}), 400
         
-    #     userItem = response['Items'][0]
-    #     accInfo = userItem['acc-info']
-    #     storedHashEncoded = accInfo['password']
+        userItem = response['Items'][0]
+        accInfo = userItem['acc-info']
+        storedHashEncoded = accInfo['password']
 
-    #     #Decoding the base64-encoded hash to get original byte format
-    #     storedHash = base64.b64decode(storedHashEncoded)
+        #Decoding the base64-encoded hash to get original byte format
+        storedHash = base64.b64decode(storedHashEncoded)
 
-    #     #Verifying the password through hash comparison
-    #     if not verify_password(storedHash, password):
-    #         return jsonify({'error': 'Invalid credentials'}), 400
+        #Verifying the password through hash comparison
+        if not verify_password(storedHash, password):
+            return jsonify({'error': 'Invalid credentials'}), 400
         
-    #     #Store user info in session
-    #     session['user_id'] = userItem['uuid']
+        #Store user info in session
+        session['user_id'] = userItem['uuid']
 
-    #     #return render_template('dashboard.html')
-    #     return jsonify({'Status': 'Success', 'Code': '200 OK'}), 200
-    # except ClientError as e:
-    #     return jsonify({'error': 'Error verifying user'}), 500
+        #return render_template('dashboard.html')
+        return jsonify({'Status': 'Success', 'Code': '200 OK'}), 200
+    except ClientError as e:
+        return jsonify({'error': 'Error verifying user'}), 500
 
 #Route for creating a new user
 @frontend_bp.route("/signup", methods=["POST"])
@@ -161,49 +161,49 @@ def signup():
     password = request.json.get('password')
     print(f"Email: {email}, Password: {password}")
     
-    return jsonify({'email': email, 'password': password}), 200
+    # return jsonify({'email': email, 'password': password}), 200
 
-    # if not email or not password:
-    #     return jsonify({'error': 'Missing required fields'}), 400
+    if not email or not password:
+        return jsonify({'error': 'Missing required fields'}), 400
     
-    # #Checking if email already exists
-    # try:
-    #     response = table.scan(
-    #         FilterExpression=Attr('acc-info.email').eq(email)
-    #     )
+    #Checking if email already exists
+    try:
+        response = table.scan(
+            FilterExpression=Attr('acc-info.email').eq(email)
+        )
 
-    #     if 'Items' in response and len(response['Items']) > 0:
-    #         return jsonify({'error': 'Email already exists'}), 400
-    # except ClientError as e:
-    #     return jsonify({'error': 'Error checking email existence'}), 500
+        if 'Items' in response and len(response['Items']) > 0:
+            return jsonify({'error': 'Email already exists'}), 400
+    except ClientError as e:
+        return jsonify({'error': 'Error checking email existence'}), 500
     
-    # #Hasing the password
-    # hashedPassword = hash_password(password)
+    #Hasing the password
+    hashedPassword = hash_password(password)
 
-    # #Encoding the hash to store in Dynamo as a string
-    # hashedPasswordEncoded = base64.b64encode(hashedPassword).decode('utf-8')
+    #Encoding the hash to store in Dynamo as a string
+    hashedPasswordEncoded = base64.b64encode(hashedPassword).decode('utf-8')
 
-    # #Create user object
-    # userId = generate_uuid()
-    # userItem = {
-    #     'uuid': userId,
-    #     'group': 'user',
-    #     'acc-info': {
-    #             'password': hashedPasswordEncoded,
-    #             'email': email,
-    #             'date-created': get_current_date()
-    #     }
-    # }
+    #Create user object
+    userId = generate_uuid()
+    userItem = {
+        'uuid': userId,
+        'group': 'user',
+        'acc-info': {
+                'email': email,
+                'password': hashedPasswordEncoded,
+                'date-created': get_current_date()
+        }
+    }
 
-    # #Insert into DynamoDB
-    # try:
-    #     table.put_item(Item=userItem)
-    #     print(userItem)
-    #     #return  render_template('login.html')
-    #     return jsonify({'Status': 'Success', 'Code': '200 OK'}), 200
-    # except ClientError as e:
-    #     frontend_bp.logger.error(f"DynamoDB Error: {e}")
-    #     return jsonify({'error': 'Error creating user'}), 500
+    #Insert into DynamoDB
+    try:
+        table.put_item(Item=userItem)
+        print(userItem)
+        #return  render_template('login.html')
+        return jsonify({'Status': 'Success', 'Code': '200 OK'}), 200
+    except ClientError as e:
+        frontend_bp.logger.error(f"DynamoDB Error: {e}")
+        return jsonify({'error': 'Error creating user'}), 500
 
 #Rout for logging out
 @frontend_bp.route("/logout")

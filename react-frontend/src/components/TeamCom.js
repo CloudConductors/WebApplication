71f58f907b891f9fd853e418345d6dd4e93b3650
“bryonna <bgray17@uncc.edu>
2025-04-22 12:46:30 -0400

    Added Team info

--- react-frontend/src/components/TeamCom.js
+++ react-frontend/src/components/TeamCom.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../assets/Style/team.css";
+import Bryonna from '../assets/images/Bryonna.jpg';
+import Chris from '../assets/images/Chris.jpg';
+import Devin from '../assets/images/Devin.jpg';
+import Jakob from '../assets/images/Jakob.jpg';
+import Nick from '../assets/images/Nick.jpg';
+import Troy from '../assets/images/Troy.jpg';
+import linkedin from '../assets/images/linkedin.png';
+import github from '../assets/images/github.png';
+
 
 export default function TeamCom() {
   return (
@@ -34,37 +43,67 @@ export default function TeamCom() {
           <div className="row">
             <div className="col-md-4 mb-4">
               <div className="team-card card p-3">
+                <img className="profile-img" src={Nick} alt="Nick Petruccelli" />
                 <h5 className="team-name">Nick Petruccelli</h5>
+                <div className="link-container">
+                <a href="https://www.linkedin.com/in/nick-petruccelli-b8717625a/"><img className="profile-link" src={linkedin} alt="linkedin profile link" /></a>
+                <a href="https://github.com/Nick-Petruccelli"><img className="profile-link" src={github} alt="github profile link" /></a>
+                </div>
                 <p>---</p>
               </div>
             </div>
             <div className="col-md-4 mb-4">
               <div className="team-card card p-3">
+                <img className="profile-img" src={Jakob} alt="Jakob Olsen" />
                 <h5 className="team-name">Jakob Olsen</h5>
+                <div className="link-container">
+                <a href="https://www.linkedin.com/in/jakob-c-olsen/"><img className="profile-link" src={linkedin} alt="linkedin profile link" /></a>
+                <a href="https://github.com/jakobcolsen"><img className="profile-link" src={github} alt="github profile link" /></a>
+                </div>
                 <p>---</p>
               </div>
             </div>
             <div className="col-md-4 mb-4">
               <div className="team-card card p-3">
+                <img className="profile-img" src={Devin} alt="Devin Thompson" />
                 <h5 className="team-name">Devin Thompson</h5>
+                <div className="link-container">
+                <a href="https://www.linkedin.com/in/devin-thompson-1a098522b/"><img className="profile-link" src={linkedin} alt="linkedin profile link" /></a>
+                <a href="https://github.com/dthom118"><img className="profile-link" src={github} alt="github profile link" /></a>
+                </div>
                 <p>----</p>
               </div>
             </div>
             <div className="col-md-4 mb-4">
               <div className="team-card card p-3">
+                <img className="profile-img" src={Troy} alt="Troy Brown" />
                 <h5 className="team-name">Troy Brown</h5>
+                <div className="link-container">
+                <a href="https://www.linkedin.com/in/troy-brown-836b5b289/"><img className="profile-link" src={linkedin} alt="linkedin profile link" /></a>
+                <a href="https://github.com/troyfbrown"><img className="profile-link" src={github} alt="github profile link" /></a>
+                </div>
                 <p>---</p>
               </div>
             </div>
             <div className="col-md-4 mb-4">
               <div className="team-card card p-3">
+                <img className="profile-img" src={Chris} alt="Christopher Cuartas" />
                 <h5 className="team-name">Christopher Cuartas</h5>
+                <div className="link-container">
+                <a href="https://www.linkedin.com/in/cuartas-christopher/"><img className="profile-link" src={linkedin} alt="linkedin profile link" /></a>
+                <a href="https://github.com/Zzz4nder"><img className="profile-link" src={github} alt="github profile link" /></a>
+                </div>
                 <p>---</p>
               </div>
             </div>
             <div className="col-md-4 mb-4">
               <div className="team-card card p-3">
+                <img className="profile-img" src={Bryonna} alt="Bryonna Gray" />
                 <h5 className="team-name">Bryonna Gray</h5>
+                <div className="link-container">
+                <a href="https://www.linkedin.com/in/bryonna-gray-7ab960171/"><img className="profile-link" src={linkedin} alt="linkedin profile link" /></a>
+                <a href="https://github.com/GGray1919"><img className="profile-link" src={github} alt="github profile link" /></a>
+                </div>
                 <p>---</p>
               </div>
             </div>
